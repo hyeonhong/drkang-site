cedfc5946a5c860e43605272a6bc4f362747623b
Hyeon Hong <hyeonshong@gmail.com>
2020-12-19 16:53:54 +0900

    Refactor firebase initialization

--- utils/auth/firebaseClient.js
+++ utils/auth/firebaseClient.js
@@ -1,23 +1,8 @@
 import { createContext, useContext, useState, useEffect } from 'react'
-import firebase from 'firebase/app'
-import 'firebase/auth'
 import { Typography } from '@material-ui/core'
 import queryString from 'query-string'
 import Cookies from 'js-cookie'
-
-const config = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-}
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(config)
-  firebase.auth().useDeviceLanguage()
-}
+import firebase from './initFirebase'
 
 const AuthContext = createContext()
 
