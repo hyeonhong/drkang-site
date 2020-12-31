73abe5e44d6a72aaf6551082fa1273a886e5ba72
Hyeon Hong <hyeonshong@gmail.com>
2020-12-31 10:15:11 +0900

    Add CompleteProfile

---
+++ pages/completeprofile.js
@@ -0,0 +1,118 @@
+import { useRouter } from 'next/router'
+import { Formik, Form, Field } from 'formik'
+import {
+  TextField,
+  Button,
+  Typography,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  Autocomplete
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import * as yup from 'yup'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1)
+    }
+  },
+  main: {
+    textAlign: 'center'
+    // width: '460px'
+    // padding: theme.spacing(4.5, 5)
+  },
+  form: {
+    textAlign: 'left',
+    display: 'flex',
+    flexDirection: 'column'
+    // width: '460px',
+    // padding: theme.spacing(4.5, 5)
+  }
+}))
+
+export default function CompleteProfile() {
+  const router = useRouter()
+  const classes = useStyles()
+
+  const validationSchema = yup.object({
+    firstName: yup.string().required('Required').email('Invalid email format'),
+    lastName: yup.string().required('Required')
+  })
+
+  const currentYear = new Date().getFullYear()
+  const birthYears = Array.from({ length: 100 }, (_, i) => (currentYear - i).toString())
+
+  return (
+    <div className={classes.root}>
+      <Formik
+        initialValues={{ firstName: '', lastName: '' }}
+        // validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting }) => {}}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          setFieldValue
+        }) => (
+          <Form className={classes.form}>
+            <Typography variant="h6">{'Complete My Profile'}</Typography>
+            <div>
+              <Field name="firstName">
+                {({ field }) => (
+                  <TextField {...field} required label="First Name" variant="outlined" />
+                )}
+              </Field>
+              <Field name="lastName">
+                {({ field }) => (
+                  <TextField {...field} required label="Last Name" variant="outlined" />
+                )}
+              </Field>
+            </div>
+            <RadioGroup row>
+              <Field name="gender">
+                {({ field }) => (
+                  <FormControlLabel {...field} value="male" control={<Radio />} label="Male" />
+                )}
+              </Field>
+              <Field name="gender">
+                {({ field }) => (
+                  <FormControlLabel {...field} value="female" control={<Radio />} label="Female" />
+                )}
+              </Field>
+              <Field name="gender">
+                {({ field }) => (
+                  <FormControlLabel {...field} value="other" control={<Radio />} label="Other" />
+                )}
+              </Field>
+            </RadioGroup>
+
+            <Autocomplete
+              disablePortal
+              options={birthYears}
+              onChange={(e, value) => setFieldValue('birthYears', value)}
+              style={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="Birth Year" />}
+            />
+
+            <Button disabled={isSubmitting} type="submit">
+              Submit
+            </Button>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
+}
--- pages/welcome.js
+++ pages/welcome.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { Button, Typography } from '@material-ui/core'
+import { Box, Button, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
@@ -13,14 +13,10 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1)
     }
   },
-  form: {
-    textAlign: 'left',
-    display: 'inline-block',
-    width: '460px',
-    padding: theme.spacing(4.5, 5)
-  },
-  textField: {
-    width: '100%'
+  main: {
+    textAlign: 'center'
+    // width: '460px'
+    // padding: theme.spacing(4.5, 5)
   }
 }))
 
@@ -29,22 +25,26 @@ export default function Welcome() {
   const classes = useStyles()
 
   return (
-    <>
-      <Typography variant="h4"> {'Welcome to Kang Clinic !'}</Typography>
-      <Typography variant="h6"> {'We are excited to have you on board!'}</Typography>
-      <Typography variant="h6"> {"Let's set up your profile and get you started."}</Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        disableElevation
-        disableRipple
-        disableFocusRipple
-        disableTouchRipple
-        onClick={() => router.push('/completeprofile')}
-        className={classes.lowercase}
-      >
-        Complete My Profile
-      </Button>
-    </>
+    <div className={classes.root}>
+      <div className={classes.main}>
+        <Typography variant="h4"> {'Welcome to Kang Clinic !'}</Typography>
+        <Box />
+        <Typography variant="h6"> {'We are excited to have you on board!'}</Typography>
+        <Box />
+        <Typography variant="h6"> {"Let's set up your profile and get you started."}</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          disableElevation
+          disableRipple
+          disableFocusRipple
+          disableTouchRipple
+          onClick={() => router.push('/completeprofile')}
+          className={classes.lowercase}
+        >
+          Complete My Profile
+        </Button>
+      </div>
+    </div>
   )
 }
