1f504b3ec31ef7970b988368ee1ee75cadac6df6
Hyeon Hong <hyeonshong@gmail.com>
2021-01-04 20:08:41 +0900

    Add mdx provider, remove useCallback in auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
+/* eslint-disable react/display-name */
 import 'styles/globals.css'
 
 import { useEffect } from 'react'
 import Head from 'next/head'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { CacheProvider } from '@emotion/react'
-import CssBaseline from '@material-ui/core/CssBaseline'
+import { CssBaseline, Typography } from '@material-ui/core'
 import createCache from '@emotion/cache'
+import { MDXProvider } from '@mdx-js/react'
 
 import { AuthProvider } from 'utils/auth/firebaseClient'
 import { LangProvider } from 'utils/hooks/useLang'
@@ -16,6 +18,16 @@ export const cache = createCache({ key: 'css', prepend: true })
 
 export default function MyApp(props) {
   const { Component, pageProps } = props
+
+  const mdxComponents = {
+    h1: (props) => <Typography variant={'h1'} {...props} />,
+    h2: (props) => <Typography variant={'h2'} {...props} />,
+    h3: (props) => <Typography variant={'h3'} {...props} />,
+    h4: (props) => <Typography variant={'h4'} {...props} />,
+    h5: (props) => <Typography variant={'h5'} {...props} />,
+    h6: (props) => <Typography variant={'h6'} {...props} />,
+    p: (props) => <Typography variant={'body1'} {...props} />
+  }
 
   useEffect(() => {
     // Remove the server-side injected CSS.
@@ -34,9 +46,11 @@ export default function MyApp(props) {
         <CssBaseline />
         <AuthProvider>
           <LangProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
+            <MDXProvider components={mdxComponents}>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </MDXProvider>
           </LangProvider>
         </AuthProvider>
       </ThemeProvider>
