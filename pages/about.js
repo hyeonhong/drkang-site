b08db01dd7f7f32b3892b9c40ae2a0606b11122d
Hyeon Hong <hyeonshong@gmail.com>
2021-01-04 20:37:00 +0900

    Remove the broken link in /about page

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,3 @@
-import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
-import Link from 'components/Link'
-
 export default function About() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Button variant="contained" color="primary" component={Link} naked href="/">
-          Go to the main page
-        </Button>
-      </Box>
-    </Container>
-  )
+  return <div>About page</div>
 }
