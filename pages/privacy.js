df1e35459a4f66f2467f814cbf7d04d0e7746cdf
Hyeon Hong <hyeonshong@gmail.com>
2021-01-07 14:21:25 +0900

    Add privacy policy

--- pages/privacy.js
+++ pages/privacy.js
@@ -3,15 +3,15 @@
 import dynamic from 'next/dynamic'
 import { useLang } from 'utils/hooks/useLang'
 
-export default function PrivacyPolicy() {
+export default function Privacy() {
   const { lang } = useLang()
 
-  const PrivacyPolicyKr = dynamic(() => import('mdx/privacyPolicyKr.mdx'), {
+  const PrivacyPolicyKr = dynamic(() => import('mdx/privacyKr.mdx'), {
     loading: () => <p>Loading ...</p>,
     ssr: false
   })
 
-  const PrivacyPolicyEn = dynamic(() => import('mdx/privacyPolicyEn.mdx'), {
+  const PrivacyPolicyEn = dynamic(() => import('mdx/privacyEn.mdx'), {
     loading: () => <p>Loading ...</p>,
     ssr: false
   })
