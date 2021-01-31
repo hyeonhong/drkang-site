ae50d6b6de58610edb05e36a33405ecf64d8051e
Hyeon Hong <hyeonshong@gmail.com>
2021-01-31 17:44:16 +0900

    Add directions page

---
+++ pages/directions.js
@@ -0,0 +1,37 @@
+import { Box, Typography } from '@material-ui/core'
+
+import withTexts from 'utils/hoc/withTexts'
+import Map from 'components/Map'
+
+const Directions = ({ texts }) => {
+  return (
+    <Box>
+      <Box sx={{ marginBottom: 10 }} />
+      <Typography variant="h4" sx={{ marginBottom: 8 }}>
+        {'찾아오시는 길'}
+      </Typography>
+      <Map />
+      <Box sx={{ marginBottom: 6 }} />
+      <Typography variant="h6">{'주소'}</Typography>
+      <hr />
+      <Typography variant="body1">
+        {'경기 고양시 일산동구 중앙로1275번길 38-10 우림로데오스위트 209호'}
+      </Typography>
+      <Box sx={{ marginBottom: 6 }} />
+      <Typography variant="h6">{'주차'}</Typography>
+      <hr />
+      <Typography variant="body1">
+        {'우림로데오스위트 지하 주차장(지하 1-4층) 주차 가능'}
+      </Typography>
+      <Box sx={{ marginBottom: 6 }} />
+      <Typography variant="h6">{'대중교통'}</Typography>
+      <hr />
+      <Typography variant="body1">{'지하철: 3호선 정발산역 2번출구 앞 도보 3분'}</Typography>
+      <Box sx={{ marginBottom: 10 }} />
+    </Box>
+  )
+}
+
+Directions.displayName = 'Directions'
+
+export default withTexts(Directions)
