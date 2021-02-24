c5dd9f04f80a889d32bfabc080320751d160232e
Hyeon Hong <hyeonshong@gmail.com>
2021-02-25 00:15:59 +0900

    Use responsive image for mobile

--- components/MarkdownContent.js
+++ components/MarkdownContent.js
@@ -1,12 +1,19 @@
 /* eslint-disable react/display-name */
 
-import { Typography } from '@material-ui/core'
+import Image from 'next/image'
+import { useMediaQuery, Typography } from '@material-ui/core'
 import ReactMarkdown from 'react-markdown'
 
 export default function MarkdownContent({ source }) {
+  const isMobile = useMediaQuery('(max-width:600px)')
+
   const renderers = {
-    // image: ({ src, alt, ...rest }) => <Image src={src} alt={alt} {...rest} />,
-    paragraph: (props) => <Typography variant={'body1'} {...props} />,
+    ...(isMobile && {
+      image: ({ src, alt, ...rest }) => (
+        <Image src={src} alt={alt} width={300} height={300} layout="responsive" {...rest} />
+      )
+    }),
+    paragraph: (props) => <Typography component={'div'} variant={'body1'} {...props} />,
     heading: ({ level, ...rest }) => <Typography variant={`h${level}`} {...rest} />
   }
 
--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/
 })
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  images: {
+    domains: ['drkang-media.s3.ap-northeast-2.amazonaws.com']
+  }
 })
